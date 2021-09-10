7f5eec87d216affa37272fbc1edd0f44a42aefc0
wighawag <wighawag@gmail.com>
2021-09-10 09:34:02 +0100

    Loot XP

--- web/src/routes/index.svelte
+++ web/src/routes/index.svelte
@@ -17,94 +17,62 @@
     />
 
     <p class="m-6 text-white font-extrabold text-4xl">
-      Grab your best Loot and compete with other Looters
+      Play Games with your Loot and get XP!
     </p>
   </div>
 
-  <p class="mx-8">lootxp is a battle game for <a href="https://lootproject.com">Loot</a></p>
-  <p>Each Loot token represent a set of card that you have to strategicly play to win!</p>
+  <p class="mx-8 mb-3">XP (for Adventurers) is a XP system built for <a href="https://lootproject.com">Loot</a></p>
+  <p class="mx-8">Each Loot has now an associated XP score. See <a class="underline" href={`https://etherscan.io/address/${contractsInfo.contracts.LootXPRegistry.address}`}>contract</a></p>
+  <p class="mx-8">Xp is given through participation in on-chain game with sound game mechanics,</p>
 
 
   <h2 class="mx-8 mt-12 mb-8 font-bold text-2xl text-gray-200">
-    3 Versions for Loot, MLoot and Synthetic Loot
+    <a class="underline" href="https://yooloot.xyz">YooLoot</a>
   </h2>
 
-  <div class="max-w-md mx-auto pt-1 mb-5 space-y-3 md:mt-8 md:space-y-5">
-    <div class="space-y-5 sm:flex sm:justify-center sm:space-y-0 sm:space-x-3">
-      <NavButton blank={true} class="big secondary" href="https://loot.lootxp.xyz" label="The Original Loot">Loot</NavButton>
-      <NavButton
-        blank={true}
-        class="big secondary"
-        href="https://mloot.lootxp.xyz"
-        label="MLoot"
-      >
-        MLoot
-      </NavButton>
-      <NavButton
-        blank={true}
-        class="big secondary"
-        href="https://everyone.lootxp.xyz"
-        label="Synthetic Loot"
-      >
-        Synthetic
-      </NavButton>
+  <p class="mx-8"><a class="underline" href="https://yooloot.xyz">YooLoot</a> is the first game using that system. By playing with your Loot, your Loot will potentially get XP.</p>
+  <p class="mx-8">This XP can in turn be used by other games.</p>
+
+
+  <h2 class="mx-8 mt-12 mb-8 font-bold text-2xl text-gray-200">
+    The Loot Gods DAO
+  </h2>
+
+  <p class="mx-8">In order to ensure XP sournces and sync and logically sound, the Loot XP sources (Game that can generate XP is governed by a DAO.</p>
+
+  <p class="mx-8 mt-8">Come have a chat on our Discord server!</p>
+  <a href="https://discord.gg/yrRSNNCysZ" target="_blank" class="mx-auto block w-min discord" >
+  <svg class="h-10 w-26 block" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 800 272.1"><path class="st0" d="M142.8 120.1c-5.7 0-10.2 4.9-10.2 11s4.6 11 10.2 11c5.7 0 10.2-4.9 10.2-11s-4.6-11-10.2-11zM106.3 120.1c-5.7 0-10.2 4.9-10.2 11s4.6 11 10.2 11c5.7 0 10.2-4.9 10.2-11 .1-6.1-4.5-11-10.2-11z"/><path class="st0" d="M191.4 36.9h-134c-11.3 0-20.5 9.2-20.5 20.5v134c0 11.3 9.2 20.5 20.5 20.5h113.4l-5.3-18.3 12.8 11.8 12.1 11.1 21.6 18.7V57.4c-.1-11.3-9.3-20.5-20.6-20.5zm-38.6 129.5s-3.6-4.3-6.6-8c13.1-3.7 18.1-11.8 18.1-11.8-4.1 2.7-8 4.6-11.5 5.9-5 2.1-9.8 3.4-14.5 4.3-9.6 1.8-18.4 1.3-25.9-.1-5.7-1.1-10.6-2.6-14.7-4.3-2.3-.9-4.8-2-7.3-3.4-.3-.2-.6-.3-.9-.5-.2-.1-.3-.2-.4-.2-1.8-1-2.8-1.7-2.8-1.7s4.8 7.9 17.5 11.7c-3 3.8-6.7 8.2-6.7 8.2-22.1-.7-30.5-15.1-30.5-15.1 0-31.9 14.4-57.8 14.4-57.8 14.4-10.7 28-10.4 28-10.4l1 1.2c-18 5.1-26.2 13-26.2 13s2.2-1.2 5.9-2.8c10.7-4.7 19.2-5.9 22.7-6.3.6-.1 1.1-.2 1.7-.2 6.1-.8 13-1 20.2-.2 9.5 1.1 19.7 3.9 30.1 9.5 0 0-7.9-7.5-24.9-12.6l1.4-1.6s13.7-.3 28 10.4c0 0 14.4 25.9 14.4 57.8 0-.1-8.4 14.3-30.5 15zM303.8 79.7h-33.2V117l22.1 19.9v-36.2h11.8c7.5 0 11.2 3.6 11.2 9.4v27.7c0 5.8-3.5 9.7-11.2 9.7h-34v21.1h33.2c17.8.1 34.5-8.8 34.5-29.2v-29.8c.1-20.8-16.6-29.9-34.4-29.9zm174 59.7v-30.6c0-11 19.8-13.5 25.8-2.5l18.3-7.4c-7.2-15.8-20.3-20.4-31.2-20.4-17.8 0-35.4 10.3-35.4 30.3v30.6c0 20.2 17.6 30.3 35 30.3 11.2 0 24.6-5.5 32-19.9l-19.6-9c-4.8 12.3-24.9 9.3-24.9-1.4zM417.3 113c-6.9-1.5-11.5-4-11.8-8.3.4-10.3 16.3-10.7 25.6-.8l14.7-11.3c-9.2-11.2-19.6-14.2-30.3-14.2-16.3 0-32.1 9.2-32.1 26.6 0 16.9 13 26 27.3 28.2 7.3 1 15.4 3.9 15.2 8.9-.6 9.5-20.2 9-29.1-1.8l-14.2 13.3c8.3 10.7 19.6 16.1 30.2 16.1 16.3 0 34.4-9.4 35.1-26.6 1-21.7-14.8-27.2-30.6-30.1zm-67 55.5h22.4V79.7h-22.4v88.8zM728 79.7h-33.2V117l22.1 19.9v-36.2h11.8c7.5 0 11.2 3.6 11.2 9.4v27.7c0 5.8-3.5 9.7-11.2 9.7h-34v21.1H728c17.8.1 34.5-8.8 34.5-29.2v-29.8c0-20.8-16.7-29.9-34.5-29.9zm-162.9-1.2c-18.4 0-36.7 10-36.7 30.5v30.3c0 20.3 18.4 30.5 36.9 30.5 18.4 0 36.7-10.2 36.7-30.5V109c0-20.4-18.5-30.5-36.9-30.5zm14.4 60.8c0 6.4-7.2 9.7-14.3 9.7-7.2 0-14.4-3.1-14.4-9.7V109c0-6.5 7-10 14-10 7.3 0 14.7 3.1 14.7 10v30.3zM682.4 109c-.5-20.8-14.7-29.2-33-29.2h-35.5v88.8h22.7v-28.2h4l20.6 28.2h28L665 138.1c10.7-3.4 17.4-12.7 17.4-29.1zm-32.6 12h-13.2v-20.3h13.2c14.1 0 14.1 20.3 0 20.3z"/></svg>
+  </a>
+
+</div>
+
+
+
+<footer class="bg-black" aria-labelledby="footerHeading">
+  <div class="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8">
+    <div class="mt-8 border-t border-gray-700 pt-8 md:flex md:items-center md:justify-between">
+      <div class="flex space-x-6 md:order-2">
+
+        <a href="https://twitter.com/lootgodsDAO" target="_blank" class="text-gray-400 hover:text-gray-300">
+          <span class="sr-only">Twitter</span>
+          <svg class="h-6 w-6" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
+            <path d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84" />
+          </svg>
+        </a>
+
+        <a href="https://github.com/wighawag/lootxp"  target="_blank" class="text-gray-400 hover:text-gray-300">
+          <span class="sr-only">GitHub</span>
+          <svg class="h-6 w-6" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
+            <path fill-rule="evenodd" d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z" clip-rule="evenodd" />
+          </svg>
+        </a>
+
+        <a href="https://discord.gg/yrRSNNCysZ" target="_blank" class="text-gray-400 hover:text-gray-300">
+          <span class="sr-only">Discord</span>
+          <svg class="w-7 h-7" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 245 240" fill="currentColor"><path class="st0" d="M104.4 103.9c-5.7 0-10.2 5-10.2 11.1s4.6 11.1 10.2 11.1c5.7 0 10.2-5 10.2-11.1.1-6.1-4.5-11.1-10.2-11.1zM140.9 103.9c-5.7 0-10.2 5-10.2 11.1s4.6 11.1 10.2 11.1c5.7 0 10.2-5 10.2-11.1s-4.5-11.1-10.2-11.1z"/><path class="st0" d="M189.5 20h-134C44.2 20 35 29.2 35 40.6v135.2c0 11.4 9.2 20.6 20.5 20.6h113.4l-5.3-18.5 12.8 11.9 12.1 11.2 21.5 19V40.6c0-11.4-9.2-20.6-20.5-20.6zm-38.6 130.6s-3.6-4.3-6.6-8.1c13.1-3.7 18.1-11.9 18.1-11.9-4.1 2.7-8 4.6-11.5 5.9-5 2.1-9.8 3.5-14.5 4.3-9.6 1.8-18.4 1.3-25.9-.1-5.7-1.1-10.6-2.7-14.7-4.3-2.3-.9-4.8-2-7.3-3.4-.3-.2-.6-.3-.9-.5-.2-.1-.3-.2-.4-.3-1.8-1-2.8-1.7-2.8-1.7s4.8 8 17.5 11.8c-3 3.8-6.7 8.3-6.7 8.3-22.1-.7-30.5-15.2-30.5-15.2 0-32.2 14.4-58.3 14.4-58.3 14.4-10.8 28.1-10.5 28.1-10.5l1 1.2c-18 5.2-26.3 13.1-26.3 13.1s2.2-1.2 5.9-2.9c10.7-4.7 19.2-6 22.7-6.3.6-.1 1.1-.2 1.7-.2 6.1-.8 13-1 20.2-.2 9.5 1.1 19.7 3.9 30.1 9.6 0 0-7.9-7.5-24.9-12.7l1.4-1.6s13.7-.3 28.1 10.5c0 0 14.4 26.1 14.4 58.3 0 0-8.5 14.5-30.6 15.2z"/></svg>
+        </a>
+      </div>
     </div>
   </div>
-
-
-  <h2 class="mx-8 mt-12 mb-8 font-bold text-2xl text-gray-200">
-    Fully onchain
-  </h2>
-
-  <p class="mx-8">The game fully run on-chain. All resolution is resolved through lootxp Smart contract.</p>
-  <p class="mx-8">Note that the game is experimental and we do not take any responsibility in potential bug lurking.</p>
-
-  <h2 class="mx-8 mt-12 mb-8 font-bold text-2xl text-gray-200">
-    XP (For adventurers)
-  </h2>
-
-  <p class="mx-8">The game is part of a larger ecosystem, that of "Xp (For Adventurers)".</p>
-  <p class="mx-8">For every battle a loot is involved it get given some XP.</p>
-  <p class="mx-8">The XP can then be used by other project and more game can be buidl to give these XP.</p>
-  <p class="mx-8">The DMDAO (Dungeon Master DAO) will be responsible to allocate XP sink and source through the LootXPRegistry.</p>
-
-</div>
-
-
-
-<div
-  class="border-t-2 border-gray-400 mt-16 w-full h-full mx-auto flex text-center flex-col items-center justify-center text-gray-400 ">
-  <p class="m-8">
-    Note that this project has not been audited and we do not take any responsability of lost loot.
-  </p>
-
-  <p class="p-1">
-    The source code can be found
-    <a
-      href="https://github.com/wighawag/lootxp"
-      target="_blank"
-      class="underline">here</a>
-    and the contract address is
-    <a
-      href={`https://etherscan.io/address/${contractsInfo.contracts.lootxp?.address}`}
-      target="_blank"
-      class="underline">{contractsInfo.contracts.lootxp?.address}</a>
-  </p>
-
-  <p class="p-1">
-    This project was created by
-    <a href="https://ronan.eth.link" target="_blank" class="underline">Ronan
-      Sandford</a>
-    (a.k.a
-    <a
-      href="https://twitter.com/wighawag"
-      target="_blank"
-      class="underline">wighawag
-      <img
-        class="w-4 h-4 inline"
-        alt="wighawag"
-        src={`${base}images/wig256x256.jpg`} /></a>)
-  </p>
-
-  <p class="p-1">Web Design by <a class="underline" target="_blank" href="https://vanhullebus.ch/">Buche</a> (<a class="underline" target="_blank" href="https://twitter.com/BucheMakesGames">@BucheMakesGames</a>) </p>
-</div>
+</footer>
